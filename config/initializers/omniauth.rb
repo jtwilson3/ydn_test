19e93afb278d991a0dafe69eef43507cdfd91a88
James Wilson <jtyler.wilson3@gmail.com>
2014-07-07 00:14:42 -0700

    first commit

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :yahoo, 'dj0yJmk9MWFDVHdDR3JhQ1lCJmQ9WVdrOVVFTjJSa042TkRRbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1jYg--', '21e1659aaf92cffda8e66f5aab1608318f2ca04c'
+end
