b4d685448783e02e9adc8db9f4b644521599dc75
James Wilson <jtyler.wilson3@gmail.com>
2014-07-07 11:19:45 -0700

    try desktop auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :yahoo, 'dj0yJmk9VFYxUG9NSHdKYzVrJmQ9WVdrOWIwUjBielJxTkdVbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD02NQ--', '13fd689a53da856127fc437338b915abd22c6c96'
+  provider :yahoo, 'dj0yJmk9TGRMejB5ODcyTndHJmQ9WVdrOU16TlBOR3BSTjJjbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD04Mg--', 'f0bf8cc69fee09d8049542a006a4e1ec706497e4'
 end
